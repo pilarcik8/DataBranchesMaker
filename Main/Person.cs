a90f3b2cb5d15962246d13fded624ae923eb4aed
stick <sompilarcikmichal@gmail.com>
2025-11-30 16:07:15 +0100

    Add files via upload
    Finnaly uploading to a repo, because I may destroy this code in a minute

---
+++ Main/Person.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using Bogus;
+
+namespace TestKniznice
+{
+    public class Person
+    {
+        public string Title { get; set; } //0
+        public string FirstName { get; set; } //1
+        public string LastName { get; set; } //2
+
+        public string Email { get; set; } //3
+        public string Phone { get; set; } //4
+
+        public string Gender { get; set; }  //5
+        public int Age { get; set; } //6
+        public string Company { get; set; } //7
+        public string JobTitle { get; set; } //8
+
+        public string CreditCardNumber { get; set; } //9
+        public string Street { get; set; } //10
+        public string StreetNumber { get; set; } //11
+        public string City { get; set; } //12
+        public string County { get; set; } //13
+        public string State { get; set; } //14
+        public string ZipCode { get; set; } //15
+        public string Country { get; set; } //16
+
+        public Person()
+        {
+            Title = string.Empty;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            Email = string.Empty;
+            Phone = string.Empty;
+            Gender = string.Empty;
+            Age = 0;
+            Company = string.Empty;
+            JobTitle = string.Empty;
+            CreditCardNumber = string.Empty;
+            Street = string.Empty;
+            StreetNumber = string.Empty;
+            City = string.Empty;
+            County = string.Empty;
+            State = string.Empty;
+            ZipCode = string.Empty;
+            Country = string.Empty;
+        }
+
+        // Generic getter by index (uses switch). Index mapping listed below.
+        public T GetAtribute<T>(int index)
+        {
+            object? value = index switch
+            {
+                0 => Title,
+                1 => FirstName,
+                2 => LastName,
+                3 => Email,
+                4 => Phone,
+                5 => Gender,
+                6 => Age,
+                7 => Company,
+                8 => JobTitle,
+                9 => CreditCardNumber,
+                10 => Street,
+                11 => StreetNumber,
+                12 => City,
+                13 => County,
+                14 => State,
+                15 => ZipCode,
+                16 => Country,
+                _ => throw new ArgumentOutOfRangeException(nameof(index), index, "Invalid attribute index")
+            };
+
+            if (value == null)
+                return default!;
+
+            if (value is T t)
+                return t;
+
+            try
+            {
+                // attempt conversion for simple types (e.g., int -> string or string -> int)
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidCastException($"Cannot convert attribute at index {index} (type {value.GetType()}) to {typeof(T)}.", ex);
+            }
+        }
+
+        public Type GetTypeOfAtribute(int index)
+        {
+            return index switch
+            {
+                0 => typeof(string),
+                1 => typeof(string),
+                2 => typeof(string),
+                3 => typeof(string),
+                4 => typeof(string),
+                5 => typeof(string),
+                6 => typeof(int),
+                7 => typeof(string),
+                8 => typeof(string),
+                9 => typeof(string),
+                10 => typeof(string),
+                11 => typeof(string),
+                12 => typeof(string),
+                13 => typeof(string),
+                14 => typeof(string),
+                15 => typeof(string),
+                16 => typeof(string),
+                _ => throw new ArgumentOutOfRangeException(nameof(index), index, "Invalid attribute index")
+            };
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, new[]
+            {
+                $"Firstname: {FirstName}, Lastname: {LastName}",
+                $"Email: {Email}, Phone: {Phone}",
+                $"Age: {Age}, Gender: {Gender}",
+                $"Company: {Company}, Job: {JobTitle}",
+                $"Card: {CreditCardNumber}",
+                $"Adress: {Street} {StreetNumber}, {City}, {Country}"
+            });
+        }
+
+        internal void ChangeAttribute(int i, Faker faker)
+        {
+            if (faker == null) throw new ArgumentNullException(nameof(faker));
+
+            switch (i)
+            {
+                case 0:
+                    Title = faker.Name.Prefix();
+                    break;
+                case 1:
+                    FirstName = faker.Name.FirstName();
+                    break;
+                case 2:
+                    LastName = faker.Name.LastName();
+                    break;
+                case 3:
+                    Email = faker.Internet.Email();
+                    break;
+                case 4:
+                    Phone = faker.Phone.PhoneNumber();
+                    break;
+                case 5:
+                    Gender = faker.PickRandom(new[] { "Male", "Female", "Other" });
+                    break;
+                case 6:
+                    Age = faker.Random.Int(18, 80);
+                    break;
+                case 7:
+                    Company = faker.Company.CompanyName();
+                    break;
+                case 8:
+                    JobTitle = faker.Name.JobTitle();
+                    break;
+                case 9:
+                    CreditCardNumber = faker.Finance.CreditCardNumber();
+                    break;
+                case 10:
+                    Street = faker.Address.StreetName();
+                    break;
+                case 11:
+                    StreetNumber = faker.Address.SecondaryAddress();
+                    break;
+                case 12:
+                    City = faker.Address.City();
+                    break;
+                case 13:
+                    County = faker.Address.County();
+                    break;
+                case 14:
+                    State = faker.Address.State();
+                    break;
+                case 15:
+                    ZipCode = faker.Address.ZipCode();
+                    break;
+                case 16:
+                    Country = faker.Address.Country();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(i), i, "Invalid attribute index");
+            }
+        }
+    }
+}
+
---
+++ Main/Program.cs
@@ -0,0 +1,174 @@
+﻿using Bogus;
+using Bogus.Bson;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+
+namespace TestKniznice
+{
+    public enum AtributeAction
+    {
+        KEEP,
+        CHANGE,
+        REMOVE,
+        ADD
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Vytvorenie vyslednej osoby
+            var faker = new Faker("en");
+            Person result = CreateFakePerson(faker);
+            ExportPerson(result, "res");
+
+            // Tvorba 3 branchov
+            Person leftPerson = result;
+            Person rightPerson = result;
+            Person basePeson = result;
+
+            int atCount = typeof(Person).GetProperties().Length;
+            Stack<AtributeAction> RightAtributeActions = new Stack<AtributeAction>(atCount);
+            Stack<AtributeAction> LeftAtributeActions = new Stack<AtributeAction>(atCount);
+            Stack<AtributeAction> BaseAtributeActions = new Stack<AtributeAction>(atCount);
+
+
+            for (int i = 0; i < atCount; i++)
+            {
+                var propType = result.GetTypeOfAtribute(i);
+                object value;
+
+                AtributeAction actionR = GetAtributeAction();
+                AtributeAction actionL = GetAtributeActionWitourConfilct(actionR);
+                AtributeAction actionB = GetBaseWinningAcion(actionR, actionL);
+
+                RightAtributeActions.Push(actionR);
+                LeftAtributeActions.Push(actionL);
+                BaseAtributeActions.Push(actionB);
+
+                ExecuteAction(rightPerson, i, actionR, faker);
+                ExecuteAction(leftPerson, i, actionL, faker);
+                ExecuteAction(basePeson, i, actionL, faker);
+
+                if (propType == typeof(int))
+                {
+                    value = result.GetAtribute<int>(i);
+                }
+                else if (propType == typeof(string))
+                {
+                    value = result.GetAtribute<string>(i);
+                }
+                else
+                {
+                    string erMessage = $"Neznami typ nastal na indexe: {i} (typ: {propType.FullName})";
+                    throw new InvalidOperationException(erMessage);
+                }
+
+                Console.WriteLine($"[{i}] {propType.Name} = {value}");
+            }
+        }
+
+        private static void ExecuteAction(Person person, int i, AtributeAction action, Faker faker)
+        {
+            if (action == AtributeAction.KEEP)
+                return;
+
+            if (action == AtributeAction.CHANGE)
+            {
+                person.ChangeAttribute(i, faker);
+            }
+
+            if (action == AtributeAction.REMOVE)
+            {
+                // potrebujem odstranit dany atribut z triedy (aspon nastavit aby sa neulozil do xml ked ho expornem)
+            }
+
+            if (action == AtributeAction.ADD)
+            {
+                // potrebujem pridat novy atribut do triedy pred tento atribut (aspon nastavit aby sa ulozil do xml ked ho expornem)
+            }
+        }
+
+        // Mozu byt bud identicke alebo aspon jeden z nich musi byt KEEP
+        private static AtributeAction GetBaseWinningAcion(AtributeAction actionR, AtributeAction actionL)
+        {
+            if (actionL == AtributeAction.KEEP)
+                return actionR;
+            return actionL;
+        }
+
+        // Ak je RIGHT KEEP, tak L moze byt hocico, inak L je KEEP alebo identicke s R
+        private static AtributeAction GetAtributeActionWitourConfilct(AtributeAction actionR)
+        {
+            if (actionR == AtributeAction.KEEP)
+                return GetAtributeAction();
+
+            return Random.Shared.Next(2) == 0 ? AtributeAction.KEEP : actionR;
+        }
+
+        public static AtributeAction GetAtributeAction()
+        {
+            return (AtributeAction)new Random().Next(4);
+        }
+
+
+        private static Person CreateFakePerson(Faker faker)
+        {
+            var personFaker = new Faker<Person>("en")
+                .RuleFor(p => p.Title, f => f.Name.Prefix())
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName())
+                .RuleFor(p => p.Email, f => f.Internet.Email())
+                .RuleFor(p => p.Phone, f => f.Phone.PhoneNumber())
+                .RuleFor(p => p.Gender, f => f.PickRandom(new[] { "Male", "Female", "Other" }))
+                .RuleFor(p => p.Age, f => f.Random.Int(18, 80))
+                .RuleFor(p => p.Company, f => f.Company.CompanyName())
+                .RuleFor(p => p.JobTitle, f => f.Name.JobTitle())
+                .RuleFor(p => p.CreditCardNumber, f => f.Finance.CreditCardNumber())
+                .RuleFor(p => p.Street, f => f.Address.StreetName())
+                .RuleFor(p => p.StreetNumber, f => f.Address.SecondaryAddress())
+                .RuleFor(p => p.City, f => f.Address.City())
+                .RuleFor(p => p.County, f => f.Address.County())
+                .RuleFor(p => p.State, f => f.Address.State())
+                .RuleFor(p => p.ZipCode, f => f.Address.ZipCode())
+                .RuleFor(p => p.Country, f => f.Address.Country());                
+
+            return personFaker.Generate();
+        }
+
+        private static void ExportPerson(Person person, string fileName)
+        {
+            if (person == null)
+            {
+                Console.WriteLine("Person je null – export sa nevykoná.");
+                return;
+            }
+
+            try
+            {
+                // Relatívna cesta ku koreňu projektu
+                string projectDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+                string outputDir = Path.Combine(projectDir, "vytvoreneSubory", "Base");
+
+                // Vytvorenie priečinku, ak neexistuje
+                Directory.CreateDirectory(outputDir);
+
+                Console.WriteLine($"Výstupné súbory budú uložené do: {outputDir}");
+
+                string xmlPath = Path.Combine(outputDir, $"{fileName}.xml");
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+                using (var writer = new StreamWriter(xmlPath))
+                {
+                    xmlSerializer.Serialize(writer, person);
+                }
+                Console.WriteLine($"XML uložený do: {xmlPath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Chyba pri exporte: {ex.Message}");
+            }
+        }
+    }
+}
